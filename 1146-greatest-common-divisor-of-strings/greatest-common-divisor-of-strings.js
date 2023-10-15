b89b2c3f1118d5b0c84aa79a0a75582edc8b9cc0
sivrak22 <130675629+sivaramakrishnan-m@users.noreply.github.com>
2023-10-15 14:51:16 +0530

    Time: 184 ms (5.09%) | Memory: 49.4 MB (5.49%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    let shortString = str1.length>str2.length?str2:str1;
+    let longString = str1.length>str2.length?str1:str2;
+    let result = "";
+    for(i=1;i<=shortString.length;i++){
+        console.log(shortString.substring(0,i));
+        if((longString.replaceAll(shortString.substring(0,i),'')=='') && shortString.replaceAll(shortString.substring(0,i),'')==''){
+            result = shortString.substring(0,i);
+        }
+    }
+    return(result);
+};
