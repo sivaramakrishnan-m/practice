be92e60e60757010787833ed9bb32b5a02420a7f
sivrak22 <130675629+sivaramakrishnan-m@users.noreply.github.com>
2023-12-03 14:17:42 +0530

    Time: 46 ms (97.33%) | Memory: 42.8 MB (43.84%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,12 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-
+    for(i=0; i<nums.length-1; i++){
+        if(target===nums[i]+nums[i+1]){
+            return([i,i+1]);
+        }
+    }
     for(i=0;i<nums.length;i++){
         for(j=i+1;j<nums.length;j++){
-        if(target===nums[i]+nums[j]){
-            return([i,j]);
-        }
+            if(target===nums[i]+nums[j]){
+                return([i,j]);
+            }
         }
     }
 };
